fade81cf6cb6ca6512bc4eefbc3d54c6b70c9ef0
Jamari L Morris <jamarimorris7@gmail.com>
2022-11-22 22:44:12 -0500

    Updated render file

--- DTN_app/server/services/render.js
+++ DTN_app/server/services/render.js
@@ -1,8 +1,17 @@
+const axios = require('axios');
+
 exports.homeRoutes = (req,res) =>
 {
     res.render('index');
 }
 exports.get_data = (req,res) =>
 {
-    res.render('get_data');
+    //Make a get request to /api/data
+    axios.get('http://localhost:3000/api/data')
+        .then(function(response)
+        {
+            console.log(response)
+            res.render('get_data', { data : response.data});
+        })
+        .catch(err =>{res.send(err);})
 }
