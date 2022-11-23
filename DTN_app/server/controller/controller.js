ce69165c9371132d71b18a432e303a698d96046a
Jamari L Morris <jamarimorris7@gmail.com>
2022-11-22 21:51:41 -0500

    Update controller.js

--- DTN_app/server/controller/controller.js
+++ DTN_app/server/controller/controller.js
@@ -3,5 +3,27 @@ var Datadb = require('../model/model');
 //retrieve and return data from database
 exports.data = (req,res) =>
 {
-    
+    AltitudeSensor.data()
+    .then(data =>{res.send(data)})
+    .catch(err =>{res.status(500).send({message : err.message||"Error occured while retrieving Altitude data"})})
+
+    HumiditySensor.data()
+    .then(data =>{res.send(data)})
+    .catch(err =>{res.status(500).send({message : err.message||"Error occured while retrieving Humidity data"})})
+
+    PressureSensor.data()
+    .then(data =>{res.send(data)})
+    .catch(err =>{res.status(500).send({message : err.message||"Error occured while retrieving Pressure data"})})
+
+    RainSensor.data()
+    .then(data =>{res.send(data)})
+    .catch(err =>{res.status(500).send({message : err.message||"Error occured while retrieving Rain data"})})
+
+    TemperatureSensor.data()
+    .then(data =>{res.send(data)})
+    .catch(err =>{res.status(500).send({message : err.message||"Error occured while retrieving Temperature data"})})
+
+    UVSensor.data()
+    .then(data =>{res.send(data)})
+    .catch(err =>{res.status(500).send({message : err.message||"Error occured while retrieving UV data"})})
 }
